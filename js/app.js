1a66b2d44ffe0b1d16c311207016a331f988efa4
Виталий <vitala9b2@yahoo.com>
2024-02-28 00:46:16 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -584,7 +584,7 @@
         files_data.forEach((val => {
             if (val.id === handlElement) {
                 console.log(currentPopup.targetOpen.element.firstElementChild.firstElementChild.lastElementChild);
-                currentPopup.targetOpen.element.firstElementChild.firstElementChild.lastElementChild.innerHTML = '<div>srvervb</div>'
+                currentPopup.targetOpen.element.firstElementChild.firstElementChild.lastElementChild.textContent = 'esfgesg'
             }
         }));
     }));
